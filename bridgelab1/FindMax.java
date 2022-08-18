fcbaa7070f3342c5df36da083f6e98591119bc1c
praveen <praveenpkbadiger@gmail.com>
2022-08-18 20:58:22 +0530

    adding max string in given string

--- bridgelab1/FindMax.java
+++ bridgelab1/FindMax.java
@@ -23,10 +23,13 @@ class TestMaximum
         int i;
         Integer inums[]={10,2,5,4,6,1};
         Float f[]={2.2f,5.4f,1.6f,8.3f,4.6f,1.4f};
+        String str[] = {"praveen","vijay","arun"};
         FindMax<Integer> iob = new FindMax<>(inums);
         FindMax<Float>dob = new FindMax<Float>(f);
+        FindMax<String>sm = new FindMax<String>(str);
         System.out.println("Max value in inums: " + iob.max());
         System.out.println("Max value in float: " + dob.max());
+        System.out.println("Max string in three: " + sm.max());
 
     }
 }
